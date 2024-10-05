640a0b1d3e24ba7adb9db482c5507d94430c34a2
Abi <abiramigs21@gmail.com>
2024-10-05 12:24:49 +0530

    Project structure set up

---
+++ frontend/src/components/HeroHome/HeroHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HeroHome.css'
+
+const HeroHome = () => {
+  return (
+    <div>HeroHome</div>
+  )
+}
+
+export default HeroHome
---
+++ frontend/src/components/OpenAccount/OpenAccount.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './OpenAccount.css'
+
+const OpenAccount = () => {
+  return (
+    <div>OpenAccount</div>
+  )
+}
+
+export default OpenAccount
